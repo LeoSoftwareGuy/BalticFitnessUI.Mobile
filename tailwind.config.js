d5200d02f35c55d16ccf225e74c3bf54ea3d033f
Leonid Lobanov <leonid.lobanov@pixster.eu>
2024-11-05 21:03:40 +0200

    Fixed TabBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
          '3': '30px',
         '4': '40px', // 4 * 10px
         '5': '50px', // 5 * 10px
+        '6':'60px',
         '16':'16px'
         // Add any other spacing adjustments needed
       },
